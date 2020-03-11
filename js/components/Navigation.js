2be7e2e839943c5105db4aefc120080fb6ad2061
syrrelCabiling <35806825+syrrelCabiling@users.noreply.github.com>
2020-03-10 23:54:23 -0400

    added exit btn on users component; added a sidebar on the adults dashboard

--- js/components/Navigation.js
+++ js/components/Navigation.js
@@ -1,20 +1,46 @@
 export default {
     template: `
+    <section>
     <div class="container-fluid">
     <div class="row">
-      <header class="col-sm-12 fixed-top">
-        <img src="./images/white-lg.png" alt="Roku logo" width="90">
-        <nav class="float-right">
-          <ul>
-            <!-- <li>Kids</li> -->
-            <li v-if="authenticated"><i class="fas fa-user-circle"></i></li>
-            <li v-if="administrator"><i class="fas fa-cog"></i></li>
-            <li v-if="authenticated" v-on:click="logout()"><i class="fas fa-power-off"></i></li>
-          </ul>
-        </nav>
-      </header>
+    <header class="col-sm-12 fixed-top roku-logo  pt-3 pb-3">
+    <img src="./images/white-lg.png" alt="Roku logo">
+    <div id="sidebar">
+      <div class="burger-toggle" onclick="toggleSidebar()">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul class="mt-5">
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+        <li>Link 4</li>
+      </ul>
+    
+    <!-- Profile Nav outside -->
+    <ul>
+    <!-- <li>Kids</li> -->
+    <li v-if="authenticated"><i class="fas fa-user-circle"></i></li>
+    <li v-if="administrator"><i class="fas fa-cog"></i></li>
+    <li v-if="authenticated" v-on:click="logout()"><i class="fas fa-power-off"></i></li>
+    </ul>
+    </div>
+ 
+  </header>
     </div>
   </div> <!-- end container fluid -->
-    
-    `
+  
+  
+
+    `,
+    data() {
+      return {
+          input: {
+              username: "",
+              password: ""
+          },
+
+      }
+  }
 }
